e15669175750fdd7f023b362a8bd104e5a1507eb
Dominic Marcello <dmarce504@gmail.com>
2020-02-06 11:13:26 -0600

    Adding silo source files

---
+++ octotiger/silo.hpp
@@ -0,0 +1,15 @@
+/*
+ * silo.hpp
+ *
+ *  Created on: Feb 6, 2020
+ *      Author: dmarce1
+ */
+
+#ifndef OCTOTIGER_SILO_HPP_
+#define OCTOTIGER_SILO_HPP_
+
+
+#include <silo.h>
+
+
+#endif /* OCTOTIGER_SILO_HPP_ */
---
+++ src/silo.cpp
@@ -0,0 +1,14 @@
+/*
+ * silo.cpp
+ *
+ *  Created on: Feb 6, 2020
+ *      Author: dmarce1
+ */
+#include <octotiger/silo.hpp>
+
+#include <octotiger/fixed_real.hpp>
+
+
+
+
+
