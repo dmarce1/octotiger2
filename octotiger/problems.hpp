20c1d9d5c2edc72f033a8d203587fec7a734ef39
Dominic Marcello <dmarce504@gmail.com>
2020-02-06 21:24:46 -0600

    Added problems file

---
+++ octotiger/problems.hpp
@@ -0,0 +1,15 @@
+/*
+ * problems.hpp
+ *
+ *  Created on: Feb 6, 2020
+ *      Author: dmarce1
+ */
+
+#ifndef OCTOTIGER_PROBLEMS_HPP_
+#define OCTOTIGER_PROBLEMS_HPP_
+
+
+
+
+
+#endif /* OCTOTIGER_PROBLEMS_HPP_ */
---
+++ src/problems.cpp
@@ -0,0 +1,10 @@
+/*
+ * problems.cpp
+ *
+ *  Created on: Feb 6, 2020
+ *      Author: dmarce1
+ */
+
+#include <octotiger/problems.hpp>
+
+
